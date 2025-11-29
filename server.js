996cf8429ff4fb2a3a862c4799e4b48c3deaf01b
yibellawang-sudo <yi.bella.wang@gmail.com>
2025-11-28 19:45:41 -0500

    imported path, fileURLtoPath

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ import express from 'express';
 import cors from 'cors';
 import fetch from 'node-fetch';
 import dotenv from 'dotenv';
+import path from "path";                
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 
